a476feaf6fb18bd0928ba9c56444a7dc6fd63c51
AleksandarStojanovikj <aleksandar_sangi@hotmail.com>
2019-05-25 00:05:50 +0200

    Add routes

--- client/src/app.ts
+++ client/src/app.ts
@@ -1,3 +1,33 @@
+import { PLATFORM } from 'aurelia-pal';
+import { Router, RouterConfiguration } from 'aurelia-router'
+import { autoinject } from "aurelia-framework";
+
+@autoinject
 export class App {
-  public message: string = 'Hello World!';
+
+  constructor(private router: Router){}
+
+  public configureRouter(config: RouterConfiguration, router) {
+    config.title = 'User Management';
+    config.options.pushState = true;
+
+    config.map([
+      {
+        route: ['', 'login'],
+        name: 'login',
+        title: 'Login',
+        moduleId: PLATFORM.moduleName('public/login'),
+        nav: true
+      },
+      {
+        route: ['register'],
+        name: 'register',
+        title: 'Register',
+        moduleId: PLATFORM.moduleName('public/register'),
+        nav: true
+      }
+    ])
+
+    this.router = router;
+  }
 }
