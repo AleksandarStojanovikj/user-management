09726dbea52bd7a5d522106e7b9c3c02668f2af1
AleksandarStojanovikj <aleksandar_sangi@hotmail.com>
2019-05-27 00:53:04 +0200

    Implement user registration

--- src/main/java/com/emt/lab/usermanagement/service/UserManagementService.java
+++ src/main/java/com/emt/lab/usermanagement/service/UserManagementService.java
@@ -1,10 +1,12 @@
 package com.emt.lab.usermanagement.service;
 
+import com.emt.lab.usermanagement.model.exceptions.EmailAlreadyExistsException;
 import com.emt.lab.usermanagement.model.User;
+import com.emt.lab.usermanagement.model.dto.UserDto;
 
 public interface UserManagementService {
 
-    void registerUser(String email, String fullname, String passwordPlain);
+    User registerUser(UserDto user) throws EmailAlreadyExistsException;
 
     void verifyUser(User user);
 
--- src/main/java/com/emt/lab/usermanagement/service/impl/UserManagementServiceImpl.java
+++ src/main/java/com/emt/lab/usermanagement/service/impl/UserManagementServiceImpl.java
@@ -1,4 +1,41 @@
 package com.emt.lab.usermanagement.service.impl;
 
-public class UserManagementServiceImpl {
+import com.emt.lab.usermanagement.model.User;
+import com.emt.lab.usermanagement.model.dto.UserDto;
+import com.emt.lab.usermanagement.model.exceptions.EmailAlreadyExistsException;
+import com.emt.lab.usermanagement.repository.UserRepository;
+import com.emt.lab.usermanagement.repository.email.EmailSenderRepository;
+import com.emt.lab.usermanagement.service.UserManagementService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserManagementServiceImpl implements UserManagementService {
+
+    private final UserRepository userRepository;
+    private final EmailSenderRepository emailSenderRepository;
+
+    public UserManagementServiceImpl(UserRepository userRepository, EmailSenderRepository emailSenderRepository) {
+        this.userRepository = userRepository;
+        this.emailSenderRepository = emailSenderRepository;
+    }
+
+    @Override
+    public User registerUser(UserDto userDto) throws EmailAlreadyExistsException {
+        if (userRepository.findByEmail(userDto.email) != null)
+            throw new EmailAlreadyExistsException();
+
+        User user = User.register(userDto);
+
+        return this.userRepository.save(user);
+    }
+
+    @Override
+    public void verifyUser(User user) {
+
+    }
+
+    @Override
+    public void changeEmail() {
+
+    }
 }
